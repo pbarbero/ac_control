e184bdf81f8b876501ad0f5f7e657e08dd34fe78
Rubén Durán <ruduran@users.noreply.github.com>
2018-08-09 09:33:43 +0200

    Keep track of the button pushes

--- controller/button_pusher/models.py
+++ controller/button_pusher/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
+class ButtonPush(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET(get_sentinel_user))
+    datetime = models.DateTimeField(auto_now_add=True)
--- controller/button_pusher/views.py
+++ controller/button_pusher/views.py
@@ -5,9 +5,16 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.base import TemplateView
 
+from .models import ButtonPush
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['push_count'] = ButtonPush.objects.count()
+        return context
 
 
 def push_button(request):
@@ -24,5 +31,8 @@ def push_button(request):
 
     with serial.Serial(port, 9600) as arduino:
         arduino.write(b"{'action': 'motor'}")
+
+    button_push = ButtonPush(user=request.user)
+    button_push.save()
 
     return HttpResponseRedirect(reverse('index'))
