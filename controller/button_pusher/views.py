cc2570768b6a32340301d5d950a06090ad174e46
Rubén Durán Balda <ruduran@users.noreply.github.com>
2019-07-09 18:00:41 +0200

    PEP-8 compliance

--- controller/button_pusher/views.py
+++ controller/button_pusher/views.py
@@ -8,43 +8,42 @@ from django.views.generic.base import TemplateView
 from .models import ButtonPush
 
 
+ARDUINO_PORT = '/dev/ttyACM0'
+
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['push_count'] = ButtonPush.objects.count()
-        context['temperature'] = GetTemperature()
+        context['temperature'] = self.get_temperature()
         return context
 
+    @staticmethod
+    def get_temperature():
+        # Disable reset after hangup
+        with open(ARDUINO_PORT) as f:
+            attrs = termios.tcgetattr(f)
+            attrs[2] = attrs[2] & ~termios.HUPCL
+            termios.tcsetattr(f, termios.TCSAFLUSH, attrs)
 
+        with serial.Serial(ARDUINO_PORT, 9600) as arduino:
+            arduino.write(b"{'action': 'temperature'}")
+            return arduino.readline().strip().decode('ascii')
 
-def GetTemperature():
-    port = '/dev/ttyACM0'
-
-    # Disable reset after hangup
-    with open(port) as f:
-        attrs = termios.tcgetattr(f)
-        attrs[2] = attrs[2] & ~termios.HUPCL
-        termios.tcsetattr(f, termios.TCSAFLUSH, attrs)
-
-    with serial.Serial(port, 9600) as arduino:
-        arduino.write(b"{'action': 'temperature'}")
-        return arduino.readline().strip().decode('ascii')
 
 def push_button(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
 
-    port = '/dev/ttyACM0'
-
     # Disable reset after hangup
-    with open(port) as f:
+    with open(ARDUINO_PORT) as f:
         attrs = termios.tcgetattr(f)
         attrs[2] = attrs[2] & ~termios.HUPCL
         termios.tcsetattr(f, termios.TCSAFLUSH, attrs)
 
-    with serial.Serial(port, 9600) as arduino:
+    with serial.Serial(ARDUINO_PORT, 9600) as arduino:
         arduino.write(b"{'action': 'motor'}")
 
     button_push = ButtonPush(user=request.user)
